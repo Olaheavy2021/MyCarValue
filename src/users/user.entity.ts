c8c2dc3a16219c06e4006401c1e6fa1f96397bb1
Sunkanmi Olawuwo <Sunkanmi.Olawuwo@stanbicibtc.com>
2022-03-19 22:04:44 +0100

    Response serialization

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {AfterInsert,AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User{
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column()
     email: string;
+
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert(){
+        console.log('Inserted User with id', this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate(){
+        console.log('Update User with id', this.id);
+    }
+    @AfterRemove()
+    logRemove(){
+        console.log('Remove user with id', this.id)
+    }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from "@nestjs/common";
+import { Body,
+         Controller, 
+         Post, 
+         Get,
+         Patch,
+         Param,
+         Query, 
+         Delete, 
+         NotFoundException,
+         UseInterceptors} from "@nestjs/common";
+import { CreateUserDto } from "./dtos/create-user.dto";
+import { UpdateUserDto } from "./dtos/update-user-dto";
+import { UserService } from "./users.service";
+import { Serialize } from "src/interceptors/serilalize.interceptor";
+import { UserDto } from "./dtos/user-dto";
 
-@Controller()
-export class UsersController{}
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController{
+    constructor(private usersService: UserService ){}
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto){
+        this.usersService.create(body.email, body.password);
+    }
+    
+    //@UseInterceptors(new SerializeInterceptor(UserDto))
+    @Get('/:id')
+    async findUser(@Param('id') id : string){
+        console.log('Hanlder is running');
+        const user =await  this.usersService.findOne(parseInt(id));
+        if(!user){
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email:string){
+        return this.usersService.find(email);
+    }
+
+    @Delete(':id')
+    removeUser(@Param('id') id: string){
+        return this.usersService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id:string, @Body() body: UpdateUserDto){
+        return this.usersService.update(parseInt(id), body);
+    }
+
+
+
+
+}
